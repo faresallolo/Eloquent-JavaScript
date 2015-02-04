13942043f91a55a8eb429b56170b39ddec980b68
SexualPotatoes <lucasrizzotto@hotmail.com>
2015-02-04 02:20:48 -0500

    Create Minimum.js

---
+++ Minimum.js
@@ -0,0 +1,12 @@
+/* In this exercise we're supposed to create a function that shows the smallest of two numbers */
+
+function minimum(x, y) {
+    
+    if (x > y) {
+      return y;
+    }
+    
+    else {
+      return x;
+    }
+}
