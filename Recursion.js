b76422aa2aeb65314d52efd47fc320c8ddb1cde0
SexualPotatoes <lucasrizzotto@hotmail.com>
2015-02-04 02:27:03 -0500

    Create Recursion.js

---
+++ Recursion.js
@@ -0,0 +1,12 @@
+/* In this exercise on Chapter 3, we're supposed to create a function that says if a number is even or not */
+
+function isEven(number) {
+    
+    if (number % 2 === 0) {
+        return true;
+    }
+    
+    else {
+        return false;
+    }
+}
