5180863c4696f98aedfa537cb9b2d36e204f997e
Lucas Rizzotto <contact@lucasrizzotto.com>
2015-02-05 14:10:47 -0500

    Update SumOfARange.js

--- SumOfARange.js
+++ SumOfARange.js
@@ -1,65 +1,53 @@
-/*This one is still not completed. I'm supposed to create two functions: one that builds an array with all the numbers
-that belong to a range, and one that adds up all the numbers in that array. The Range takes in 3 values, start, end and step.
-But when the step is negative, the program breaks */
+function sum(array) {
 
-function range(start, end, step) {
-    
-    /* If no step is given, 1 is considered */
-    /* WARNING: Negative step leads to a crash */
-
-    if (step === undefined) {
-
-        var arrayStep = 1;
-    }
-    
-    else {
-        var arrayStep = step;
-    }
-    
-    
-    /* Building the array */
-
-	var arrayFinal = [];
-
-	if (start > end) {
-
-		for (var i = 0 ; i < (1 + start - end) ; i++ ) {
-
-			arrayFinal[i] = i + end;
-
-		}
-	}
-
-	else if (end > start) {
-
-		for (var i = 0 ; i < (1 + end - start) ; i++) {
-
-			arrayFinal[i] = i + start;
-		}
-	}
-	
-	/* If both numbers in the range are the same, array will have only start */
-
-	else {
-
-		arrayFinal = [start];
-	}
-
-	return arrayFinal;
-}
-
-
-function arraySum(array) {
-    
     /* Function sums up numbers in an array */
-
 	var count = 0 ;
-
 	for (var i = 0 ; i < array.length ; i++) { 
-
 		count += array[i];
-
 	}
 
 	return count;
+}
+
+function range(start, end, step) {
+
+    /* Declaring array */
+	var _numberArray = [];
+
+    /* If "step" is not defined, it's either +1 or -1, depending if the count is going up or down */
+	if (!step) {
+        
+        if (start > end) {
+            step = -1;
+        }
+        
+        else {
+		step = 1;
+        }
+	}
+
+    /* If "step" is defined, loop builds the array, using the argument "start" as a start point */
+    /* Goes up or down depending if the step is positive or negative */
+    
+	if (step < 0) {
+
+		for (var i = start ; i >= end ; i += step) {
+			_numberArray.push(i);
+		}
+	}
+
+	else { 
+		for (var i = start ; i <= end ; i += step ) {
+			_numberArray.push(i)
+		}
+	}
+	
+	/* If range is nonsensical, the array will remain empty. Delivery error message */
+	if (_numberArray[0] === undefined ) {
+	    _numberArray = "Cannnot calculate range. Remember: 1st  number is START, 2nd is END, 3rd is the STEP.";
+	}
+
+    /* Return results */
+	return _numberArray;
+
 }
